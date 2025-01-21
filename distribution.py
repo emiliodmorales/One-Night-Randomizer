918ffbae8b41d5f9908469e9ddc0f0c076526bde
Froggles <elmsterm@gmail.com>
2025-01-21 02:32:15 -0500

    moved card lists

--- distribution.py
+++ distribution.py
@@ -2,69 +2,11 @@ import math
 import random
 
 class RoleDistribution:
-    remainingRoles = {
-        "Alpha Wolf": 1,
-        "Apprentice Seer": 1,
-        "Apprentice Tanner": 1,
-        "Aura Seer": 1,
-        "Beholder": 1,
-        "Bodyguard": 1,
-        "Copycat": 1,
-        "Cursed": 1,
-        "Doppelganger": 1,
-        "Dream Wolf": 1,
-        "Drunk": 1,
-        "Gremlin": 1,
-        "Hunter": 1,
-        "Insomniac": 1,
-        "Mason": 2,
-        "Minion": 1,
-        "Mystic Wolf": 1,
-        "Paranormal Investigator": 1,
-        "Prince": 1,
-        "Revealer": 1,
-        "Robber": 1,
-        "Seer": 1,
-        "Squire": 1,
-        "Tanner": 1,
-        "Troublemaker": 1,
-        "Villager": 3,
-        "Werewolf": 2,
-        "Witch": 1,
-    }
-    roleWeights = {
-        "Alpha Wolf": 1.0,
-        "Apprentice Seer": 1.0,
-        "Apprentice Tanner": 1.0,
-        "Aura Seer": 1.0,
-        "Beholder": 1.0,
-        "Bodyguard": 1.0,
-        "Copycat": 1.0,
-        "Cursed": 1.0,
-        "Doppelganger": 1.0,
-        "Dream Wolf": 1.0,
-        "Drunk": 1.0,
-        "Gremlin": 1.0,
-        "Hunter": 1.0,
-        "Insomniac": 1.0,
-        "Mason": 2.0,
-        "Minion": 1.0,
-        "Mystic Wolf": 1.0,
-        "Paranormal Investigator": 1.0,
-        "Prince": 1.0,
-        "Revealer": 1.0,
-        "Robber": 1.0,
-        "Seer": 1.0,
-        "Squire": 1.0,
-        "Tanner": 1.0,
-        "Troublemaker": 1.0,
-        "Villager": 0.0, # excludes Villagers
-        "Werewolf": 2.0,
-        "Witch": 1.0,
-    }
+    
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, weights, deck) -> None:
+        self.roleWeights = weights
+        self.cardlist = deck
 
     def getRandomRole(self) -> str:
         r = math.floor(random.random() * self.totalWeight())
@@ -82,11 +24,65 @@ class RoleDistribution:
         return int(total)
 
     def numRoleRemaining(self, role: str) -> int:
-        return self.remainingRoles[role]
+        return self.cardlist[role]
 
     def roleAvailable(self, role: str) -> bool:
         return self.numRoleRemaining(role) > 0
 
     def takeRole(self, role: str) -> bool:
-        self.remainingRoles[role] -= 1
-        return self.remainingRoles[role] >= 0
+        self.cardlist[role] -= 1
+        return self.cardlist[role] >= 0
+    
+    def randomize(self, numPlayers: int) -> list:
+        numRoles = numPlayers + 3
+        chosenRoles = []
+        while len(chosenRoles) < numRoles:
+            # pick a random role
+            selectedRole = self.getRandomRole()
+            # If it is Mason:
+            #   There must be two open slots
+            #   It adds both Masons
+            if not self.roleAvailable(selectedRole):
+                # print(f"{selectedRole} was not available")
+                continue
+            if selectedRole == "Mason":
+                if len(chosenRoles) + 2 <= numRoles:
+                    self.takeRole(selectedRole)
+                    chosenRoles.append(selectedRole)
+
+                    self.takeRole(selectedRole)
+                    chosenRoles.append(selectedRole)
+            # If it is Alpha Wolf:
+            #   There must be a Werewolf left
+            #   It also adds a Werewolf
+            elif selectedRole == "Alpha Wolf":
+                if self.roleAvailable("Werewolf"):
+                    numRoles += 1
+
+                    self.takeRole(selectedRole)
+                    chosenRoles.append(selectedRole)
+
+                    self.takeRole("Werewolf")
+                    chosenRoles.append("Werewolf")
+            # If it is Beholder:
+            #   There must be a Seer
+            #   If there is not a Seer:
+            #       There must be two open slots
+            #       It also adds the Seer
+            elif selectedRole == "Beholder":
+                if chosenRoles.count("Seer") == 1:
+                    self.takeRole(selectedRole)
+                    chosenRoles.append(selectedRole)
+                elif len(chosenRoles) + 2 <= numRoles:
+                    self.takeRole(selectedRole)
+                    chosenRoles.append(selectedRole)
+
+                    self.takeRole("Seer")
+                    chosenRoles.append("Seer")
+            # Otherwise it adds the selected role
+            else:
+                self.takeRole(selectedRole)
+                chosenRoles.append(selectedRole)
+
+        return chosenRoles
+
--- randomizer.py
+++ randomizer.py
@@ -1,63 +1,88 @@
 import random
+import sys
 import distribution
 
+# role weight chart
+#   Always      : 999.0
+#   Often       : 1.5
+#   Standard    : 1.0
+#   Rare        : 0.6
+#   Barely      : 0.3
+#   Never       : 0.0
+
+ROLE_WEIGHTS = {
+    "Alpha Wolf": 0.9,
+    "Apprentice Seer": 1.0,
+    "Apprentice Tanner": 0.5,
+    "Aura Seer": 1.0,
+    "Beholder": 1.0,
+    "Bodyguard": 0.1,
+    "Copycat": 1.0,
+    "Cursed": 0.1,
+    "Doppelganger": 1.0,
+    "Dream Wolf": 0.5,
+    "Drunk": 1.0,
+    "Gremlin": 0.5,
+    "Hunter": 1.0,
+    "Insomniac": 1.0,
+    "Mason": 1.0, # automatically adds both masons
+    "Minion": 0.8,
+    "Mystic Wolf": 0.8,
+    "Paranormal Investigator": 1.0,
+    "Prince": 1.0,
+    "Revealer": 1.0,
+    "Robber": 1.5,
+    "Seer": 1.5,
+    "Squire": 0.8,
+    "Tanner": 0.6,
+    "Troublemaker": 0.3,
+    "Villager": 0.0, # excludes Villagers
+    "Werewolf": 2.0, # accounts for two separate werewolves
+    "Witch": 1.0,
+}
+
+CARD_LIST = {
+    "Alpha Wolf": 1,
+    "Apprentice Seer": 1,
+    "Apprentice Tanner": 1,
+    "Aura Seer": 1,
+    "Beholder": 1,
+    "Bodyguard": 1,
+    "Copycat": 1,
+    "Cursed": 1,
+    "Doppelganger": 1,
+    "Dream Wolf": 1,
+    "Drunk": 1,
+    "Gremlin": 1,
+    "Hunter": 1,
+    "Insomniac": 1,
+    "Mason": 2,
+    "Minion": 1,
+    "Mystic Wolf": 1,
+    "Paranormal Investigator": 1,
+    "Prince": 1,
+    "Revealer": 1,
+    "Robber": 1,
+    "Seer": 1,
+    "Squire": 1,
+    "Tanner": 1,
+    "Troublemaker": 1,
+    "Villager": 3,
+    "Werewolf": 2,
+    "Witch": 1,
+}
+
 def main():
-    dist = distribution.RoleDistribution()
-    numPlayers = int(input("How many players? "))
-    numRoles = numPlayers + 3
-    chosenRoles = []
-    while len(chosenRoles) < numRoles:
-        # pick a random role
-        selectedRole = dist.getRandomRole()
-        # If it is Mason:
-        #   There must be two open slots
-        #   It adds both Masons
-        if not dist.roleAvailable(selectedRole):
-            # print(f"{selectedRole} was not available")
-            continue
-        if selectedRole == "Mason":
-            if len(chosenRoles) + 2 <= numRoles:
-                dist.takeRole(selectedRole)
-                chosenRoles.append(selectedRole)
+    if len(sys.argv) == 2:
+        numPlayers = int(sys.argv[1])
+    else:
+        numPlayers = int(input("How many players? "))
 
-                dist.takeRole(selectedRole)
-                chosenRoles.append(selectedRole)
-        # If it is Alpha Wolf:
-        #   There must be a Werewolf left
-        #   It also adds a Werewolf
-        elif selectedRole == "Alpha Wolf":
-            if dist.roleAvailable("Werewolf"):
-                numRoles += 1
-
-                dist.takeRole(selectedRole)
-                chosenRoles.append(selectedRole)
-
-                dist.takeRole("Werewolf")
-                chosenRoles.append("Werewolf")
-        # If it is Beholder:
-        #   There must be a Seer
-        #   If there is not a Seer:
-        #       There must be two open slots
-        #       It also adds the Seer
-        elif selectedRole == "Beholder":
-            if chosenRoles.count("Seer") == 1:
-                dist.takeRole(selectedRole)
-                chosenRoles.append(selectedRole)
-            elif len(chosenRoles) + 2 <= numRoles:
-                dist.takeRole(selectedRole)
-                chosenRoles.append(selectedRole)
-
-                dist.takeRole("Seer")
-                chosenRoles.append("Seer")
-        # Otherwise it adds the selected role
-        else:
-            dist.takeRole(selectedRole)
-            chosenRoles.append(selectedRole)
+    dist = distribution.RoleDistribution(ROLE_WEIGHTS, CARD_LIST)
+    chosenRoles = dist.randomize(numPlayers)
     for i, role in enumerate(chosenRoles):
         print(f"{i + 1}. {role}", end="\n")
-    # prints the remaining roles
-    # print(roles)
-    return ""
+    
 
 if __name__ == "__main__":
     main()
